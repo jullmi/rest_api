9f1b4589f0962add3aeb254a23ca5d6ff83448f5
Julia Savchenko <iusavchenko@dinord.ru>
2024-05-19 22:01:41 +0400

    added list function

--- restdb.go
+++ restdb.go
@@ -45,7 +45,7 @@ func DeleteUser(ID int) bool {
 	t := FindUserID(ID)
 
 	if t.ID == 0 {
-		log.Panicln("User with ID %d does not exist", ID)
+		log.Printf("User with ID %d does not exist", ID)
 		return false
 	}
 
@@ -141,4 +141,40 @@ func IsUserValid(u User) bool {
 	}
 
 	return false
+}
+
+func ListAllUsers() []User {
+	db := ConnectPostgres()
+	if db == nil {
+		log.Println("Cannot connect to PostgreSQL")
+		db.Close()
+		return []User{}
+	}
+
+	db.Close()
+
+	rows, err := db.Query("SELECT * FROM users \n")
+	if err != nil {
+		log.Println("Query:", err)
+		return []User{}
+	}
+
+	var all = []User{}
+	var c1 int
+	var c2, c3 string
+	var c4 int64
+	var c5, c6 int
+
+	for rows.Next() {
+		err = rows.Scan(&c1, &c2, &c3, &c4, &c5, &c6)
+		if err != nil {
+			log.Println("Rows.Scan:", err)
+		}
+		temp := User{c1, c2, c3, c4, c5, c6}
+		all = append(all, temp)
+	}
+
+	log.Println(all)
+	return all
+
 }
