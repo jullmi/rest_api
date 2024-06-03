dd9418899cb065ad6fb9c1a2db330c6e7f8820c6
Julia Savchenko <iusavchenko@dinord.ru>
2024-06-03 19:05:59 +0400

    added insert user function

--- restdb.go
+++ restdb.go
@@ -357,3 +357,29 @@ func UpdateUser(u User) bool {
 	log.Println("Affected:", affect)
 	return true
 }
+
+func InsertUser(u User) bool {
+	db := ConnectPostgres()
+
+	if db == nil {
+		fmt.Println("Cannot connect to PostgreSQL!")
+		return false
+	}
+
+	defer db.Close()
+
+	if IsUserValid(u) {
+		log.Println("User", u.Username, "already exists!")
+		return false
+	}
+
+	stmt, err := db.Prepare("INSERT INTO users (Username, Password, LastLogin, Admin, Active) values($1, $2, $3, $4, $5)")
+	if err != nil {
+		log.Println("Adduser:", err)
+		return false
+	}
+
+	stmt.Exec(u.Username, u.Password, u.LastLogin, u.Admin, u.Active)
+	return true
+
+}
